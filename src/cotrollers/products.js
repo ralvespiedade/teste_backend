1abd1ca894a6d81ea439967ce8d349e56143a275
Ricardo Alves <ralvespiedade@gmail.com>
2023-10-06 11:20:04 -0300

    adding post method

--- src/cotrollers/products.js
+++ src/cotrollers/products.js
@@ -1,11 +1,43 @@
 const productsModel = require('../models/products')
 
 async function get(req, res) {
-  const products = await productsModel.find()
+  const { id } = req.params
+
+  const obj = id ? { _id: id } : null
+
+  const products = await productsModel.find(obj)
 
   res.send(products)
 }
 
+async function post(req, res) {
+  //getting new product data from body's request
+  const {
+    name,
+    price
+  } = req.body
+
+  //Verifying if some of them are null
+  if (!name || !price) {
+    res.send(`Ops! Ocorreu um erro`)
+    
+  } else {
+    //Creating new product
+      const product = new productsModel({
+      name,
+      price
+    })
+    
+    //Saving new product
+    product.save()
+  
+    res.send()
+  }
+  
+
+}
+
 module.exports = {
-  get
+  get,
+  post
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 
 const ProductController = require('../cotrollers/products')
 
-router.get('/products', ProductController.get)
+router.get('/products/:id?', ProductController.get)
+router.post('/products/', ProductController.post)
 
 module.exports = router
